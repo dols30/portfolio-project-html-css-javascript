2679345f28f2b4a826392182e0820237bf9e52af
dols30 <bashyal.dolraj30@gmail.com>
2024-05-22 21:41:28 -0500

    Add dark and light mode toggle

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
     }
+    document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        // Save preference to local storage
+        if (document.body.classList.contains('dark-mode')) {
+          localStorage.setItem('theme', 'dark');
+          document.getElementById('dark-mode-toggle').src = './assets/icon-dark.png'; // Path to dark mode icon
+    
+        } else {
+          localStorage.setItem('theme', 'light');
+          document.getElementById('dark-mode-toggle').src = './assets/icon-light.png'; // Path to dark mode icon
+    
+        }
+      });
+      
+      // Check saved preference on load
+      window.addEventListener('load', function() {
+        const theme = localStorage.getItem('theme');
+        if (theme === 'dark') {
+          document.body.classList.add('dark-mode');
+          document.getElementById('dark-mode-toggle').src = './assets/icon-dark.png'; // Path to dark mode icon
+    
+        }else{
+            document.getElementById('dark-mode-toggle').src = './assets/icon-light.png'; // Path to dark mode icon
+    
+        }
+      });
+      
